b00c045bafa4312b7abd6544c7bf017d0a91b685
irafiona <45016516+irafiona@users.noreply.github.com>
2021-11-29 13:34:44 -0500

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -88,11 +88,11 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tracer.span(name=button1)
+        tracer.span(name="Cats Vote")
 
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tracer.span(name=button2)
+        tracer.span(name="Dogs Vote")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
